ba5f90dd9e260bc061481ff62ae159f4f2f7bb86
ChangyeTange <121666744+ChangyeTange@users.noreply.github.com>
2024-11-27 14:55:19 +0800

    Update model_base.py

--- model_base.py
+++ model_base.py
@@ -1,8 +1,3 @@
-# Copyright (c) 2017, Carnegie Mellon University. All rights reserved.
-#
-# Use of the K-NRM package is subject to the terms of the software license set
-# forth in the LICENSE file included with this software, and also available at
-# https://github.com/AdeDZY/K-NRM/blob/master/LICENSE
 
 import tensorflow as tf
 import numpy as np
